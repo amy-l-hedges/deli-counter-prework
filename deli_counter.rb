b62cde15be694bf0fe0f19e4dc76d6aa0eb513d7
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 13:49:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.var katzDeliLine = []
 
-#def line(katz_deli)
- # if katz_deli.length == 0
+#def line(array)
+ # if array.length == 0
   #  puts "The line is currently empty."
   #else
 
