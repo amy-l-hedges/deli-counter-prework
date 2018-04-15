919378dae23bab10c98095c57e2017e4b80f604e
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 15:18:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
-    return "The line is currently empty."
+    break
     else
       new_array = []
       array.each_with_index do |name, index|
