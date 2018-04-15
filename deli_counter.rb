5e9184a076070d3b773c1bdf8d78ac4f0152d25e
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 13:24:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,11 +10,11 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name} you are number #{katz_deli.length}."
 end
 
-function now_serving(katz_deli) 
- if katz_deli.length == 0
-   puts "There is nobody waiting to be served!"
- else 
-   puts "Currently serving #{katz_deli.first}."
-  katz_deli.shift
-end
-end
+#function now_serving(katz_deli) 
+ #if katz_deli.length == 0
+  # puts "There is nobody waiting to be served!"
+ #else 
+#   puts "Currently serving #{katz_deli.first}."
+ # katz_deli.shift
+#end
+#end
