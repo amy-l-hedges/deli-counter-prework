4815a999042e3c2f0ebc91c33614eb1dfdc11c84
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 13:09:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,11 @@ def take_a_number(katz_deli, name)
 end
 
 function now_serving(katz_deli) 
- if katz_deli.length > 0
-   return "Currently serving #{katz_deli.shift}."
- else return "There is nobody waiting to be served!"
+ if katz_deli.length == 0
+   puts "There is nobody waiting to be served!"
+ else 
+   "Currently serving #{katz_deli.first}."
+   line.shift
 end
 end
 
-   
