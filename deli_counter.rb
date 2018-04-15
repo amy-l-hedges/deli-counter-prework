f00b008fae92cb0d97c21bf2429087832b715093
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 15:28:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 fun_array = ["Alex", "Amy"]
 sad_array = []
 
-function now_serving(array) 
+function now_serving 
  if array.length == 0
    puts "There is nobody waiting to be served!"
  else 
