0e7157edb28307a113dac1bb80f4d0fac9a1615d
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 13:53:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,17 @@
 # Write your code here.var katzDeliLine = []
 
-#def line(array)
- # if array.length == 0
-  #  puts "The line is currently empty."
-  #else
-  new_array = []
-  counter = 1 
-  array.each do |name|
-    newarray.push("#{counter}. #{name}")
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+    else
+      new_array = []
+      counter = 1
+      array.each do |name|
+        new_array.push("#{counter}. #{name}")
       counter += 1 
-      
+      puts new_array
+    end
+  end
       
       
 
