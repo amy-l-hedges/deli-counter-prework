361298f4b1b4792bcca17539d062c40d82170c1a
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 15:22:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,8 +21,8 @@ def take_a_number(array, name)
 puts "Welcome, #{array.last}. You are number #{array.length} in line."
 end
 
-#function now_serving(katz_deli) 
- #if katz_deli.length == 0
+#function now_serving(array) 
+ #if array.length == 0
   # puts "There is nobody waiting to be served!"
  #else 
 #   puts "Currently serving #{katz_deli.first}."
