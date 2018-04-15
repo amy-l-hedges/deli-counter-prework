734147abd193e0c2fc25a4d076d3e24fab9941e0
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 13:17:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ function now_serving(katz_deli)
    puts "There is nobody waiting to be served!"
  else 
    puts "Currently serving #{katz_deli.first}."
-   katz_deli.shift
 end
+  katz_deli.shift
 end
 
