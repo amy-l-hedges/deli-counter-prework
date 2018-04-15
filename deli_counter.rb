5e1c81e0867d66d09e73b9ae4b9adabec7cecbbc
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 14:19:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,8 +12,8 @@ def line(array)
       array.each do |name|
         new_array.push("#{counter}. #{name}")
       counter += 1 
-      puts "The line is currently: #{new_array}"
     end
+       puts "The line is currently: #{new_array}"   
   end
   
   line(fun_array)
