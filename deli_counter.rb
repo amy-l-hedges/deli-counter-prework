45609854d5f88e45c66b0df2756d60966305f7b5
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 13:09:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,7 @@
 # Write your code here.var katzDeliLine = []
 
+
+
 def take_a_number(katz_deli, name) 
   katz_deli.push(name)
   putz "Welcome, #{name} you are number #{katz_deli.length}."
@@ -9,7 +11,7 @@ function now_serving(katz_deli)
  if katz_deli.length == 0
    puts "There is nobody waiting to be served!"
  else 
-   "Currently serving #{katz_deli.first}."
+   puts "Currently serving #{katz_deli.first}."
    line.shift
 end
 end
